a718d499a1c25931816265c02469916cb3428eb9
anilkumar2224 <anilkumarg2224@gmail.com>
2020-11-19 15:34:02 +0530

    first commit

--- partical.js
+++ partical.js
@@ -1,6 +1,6 @@
 function draw() {
   var ctx =document.getElementById('canvas1').getContext('2d');
-if (canvas.getContext) {
+if (canvas2.getContext) {
 
   
   ctx.beginPath();
@@ -26,3 +26,37 @@ if (canvas.getContext) {
 
 }
 }
+
+var hour=document.getElementById("hour");
+var minute=document.getElementById("minute");
+var second=document.getElementById("second");
+ 
+function analogclock(){
+
+ 
+  var ctx2=document.getElementById('canvas3').getContext('2d');
+  ctx2.save();
+  ctx2.clearRect(0, 0, 400, 400);
+  ctx2.font = '17px Georgia, serif';
+                ctx2.fillStyle = 'red';
+                ctx2.fillText(' Analog-Clock', 95, 143); 
+
+                ctx2.restore();
+const day=new Date();
+const hh=day.getHours();
+const mm=day.getMinutes();
+const ss=day.getSeconds();
+
+const hdeg=(hh*30)+(mm*0.5);
+const mdeg=(mm*6)+(ss*0.1);
+const sdeg=(ss*6);
+hour.style.transform=`rotateZ(${hdeg}deg)`;
+minute.style.transform=`rotateZ(${mdeg}deg)`;
+second.style.transform=`rotateZ(${sdeg}deg)`;
+
+
+
+window.requestAnimationFrame(analogclock);
+
+}
+window.requestAnimationFrame(analogclock);
